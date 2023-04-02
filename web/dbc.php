1a228479b3bb3359970cbb072e5e05ad6c75727e
{km} <{k3042227625n@gmail.com}>
2023-04-03 05:36:43 +0900

    mysql#05関数

--- web/dbc.php
+++ web/dbc.php
@@ -1,29 +1,60 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=dot_app;charset=utf8';
-$user = 'koku';
-$pass = '00000abc';
 
-try {
-    $dbh = new PDO($dsn, $user, $pass, [
-        // エラーが出た時の例外処理
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]);
-    // echo '接続成功<br>';
+// 関数一つに一つの機能のみを持たせる
+
+// 1.データベース接続
+// 引数：なし
+// 返り値：接続結果を返す
+function dbConnect() {
+    $dsn = 'mysql:host=localhost;dbname=dot_app;charset=utf8';
+    $user = 'koku';
+    $pass = '00000abc';
+    
+    try {
+        $dbh = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);      
+    } catch (Exception $e) {
+        echo '接続失敗<br>' . $e->getMessage();
+        exit();
+    };
+
+    return $dbh;
+}
+
+echo "<br>";
+
+// 2.データを取得する
+// 引数：なし
+// 返り値：取得したデータ
+function getAllBlog() {
+    $dbh = dbConnect();
     // ①SQLの準備
     $sql = "SELECT * FROM data";
     // ②SQLの実行
     $stmt = $dbh->query($sql);
     // ③SQLの結果を受け取る
-    // 結果をカラムのみ表示(インデックス非表示)
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
     $dbh = null;
-    // var_dump($result);
-} catch (Exception $e) {
-    echo '接続失敗<br>' . $e->getMessage();
-    exit();
-};
+}
 
-echo "<br>";
+// 取得したデータを表示
+$blogData = getAllBlog();
+
+// 3.カテゴリー名を表示
+// 引数：数字
+// 返り値：カテゴリーの文字列
+function setCategoryName($category) {
+    if ($category === '1') {
+        return 'ブログ';
+    } elseif ($category === '2') {
+        return '日常';
+    } else {
+        return 'その他';
+    }
+
+}
 
 date_default_timezone_set('Asia/Tokyo');
 ?>
@@ -47,11 +78,11 @@ date_default_timezone_set('Asia/Tokyo');
             <th>タイトル</th>
             <th>カテゴリ</th>
         </tr>
-        <?php foreach ($result as $column): ?>
+        <?php foreach ($blogData as $column): ?>
         <tr>
             <td><?php echo $column['id'] ?></td>
             <td><?php echo $column['title'] ?></td>
-            <td><?php echo $column['category'] ?></td>
+            <td><?php echo setCategoryName($column['category']) ?></td>
         </tr>
         <?php endforeach; ?>
     </table>
